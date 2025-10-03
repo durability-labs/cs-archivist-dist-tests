a14a4e8ea6c24b6be996e85dbc2b748621a143b6
thatben <thatbenbierens@gmail.com>
2025-10-03 14:48:14 +0200

    Sets up module for fetching archivist network configs

---
+++ Framework/ArchivistNetworkConfig/ArchivistNetwork.cs
@@ -0,0 +1,23 @@
+﻿namespace ArchivistNetworkConfig
+{
+    public class ArchivistNetwork
+    {
+        public ArchivistVersionEntry Version { get; set; } = new ArchivistVersionEntry();
+        public ArchivistSprEntry SPR { get; set; } = new ArchivistSprEntry();
+        public string[] RPCs { get; set; } = Array.Empty<string>();
+        public ArchivistMarketplaceEntry Marketplace { get; set; } = new ArchivistMarketplaceEntry();
+        public TeamObject Team { get; set; } = new TeamObject();
+    }
+
+    public class TeamObject
+    {
+        public TeamNodesCategory[] Nodes { get; set; } = Array.Empty<TeamNodesCategory>();
+        public ArchivistNetworkTeamUtilsObject Utils { get; set; } = new ArchivistNetworkTeamUtilsObject();
+    }
+
+    public class TeamNodesCategory
+    {
+        public string Category { get; set; } = string.Empty;
+        public ArchivistNetworkTeamNodesVersionsInstancesEntry[] Instances { get; set; } = Array.Empty<ArchivistNetworkTeamNodesVersionsInstancesEntry>();
+    }
+}
---
+++ Framework/ArchivistNetworkConfig/ArchivistNetworkConnector.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Utils;
+
+namespace ArchivistNetworkConfig
+{
+    public class ArchivistNetworkConnector
+    {
+        private readonly string network;
+        private readonly string version;
+        private ArchivistNetwork? model = null;
+
+        public ArchivistNetworkConnector()
+            : this("testnet", "latest")
+        {
+        }
+
+        public ArchivistNetworkConnector(string network, string version)
+        {
+            this.network = network.ToLowerInvariant();
+            this.version = version.ToLowerInvariant();
+        }
+
+        public ArchivistNetwork GetConfig()
+        {
+            if (model == null)
+            {
+                var fullModel = FetchModel();
+                model = MapToVersion(fullModel);
+            }
+            return model;
+        }
+
+        private NetworkConfig FetchModel()
+        {
+            using var client = new HttpClient();
+            var response = Time.Wait(client.GetAsync($"https://config.archivist.storage/{network}.json"));
+            var str = Time.Wait(response.Content.ReadAsStringAsync());
+            return JsonConvert.DeserializeObject<NetworkConfig>(str)!;
+        }
+
+        private ArchivistNetwork MapToVersion(NetworkConfig fullModel)
+        {
+            var selected = GetVersion(fullModel);
+
+            return new ArchivistNetwork
+            {
+                Version = fullModel.Archivist.Single(v => v.Version == selected),
+                SPR = fullModel.SPRs.First(s => s.SupportedVersions.Contains(selected)),
+                RPCs = fullModel.RPCs,
+                Marketplace = fullModel.Marketplace.First(m => m.SupportedVersions.Contains(selected)),
+                Team = MapToVersion(fullModel.Team, selected)
+            };
+        }
+
+        private TeamObject MapToVersion(ArchivistNetworkTeamObject team, string selected)
+        {
+            return new TeamObject
+            {
+                Nodes = MapToVersion(team.Nodes, selected),
+                Utils = team.Utils
+            };
+        }
+
+        private TeamNodesCategory[] MapToVersion(ArchivistNetworkTeamNodesEntry[] nodes, string selected)
+        {
+            return nodes.Select(n => MapToVersion(n, selected)).ToArray();
+        }
+
+        private TeamNodesCategory MapToVersion(ArchivistNetworkTeamNodesEntry n, string selected)
+        {
+            return new TeamNodesCategory
+            {
+                Category = n.Category,
+                Instances = n.Versions.Single(v => v.Version == selected).Instances
+            };
+        }
+
+        private string GetVersion(NetworkConfig fullModel)
+        {
+            if (version == "latest")
+            {
+                return fullModel.Latest;
+            }
+            return version;
+        }
+    }
+}
