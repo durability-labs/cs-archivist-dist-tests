c5dedc11456bcb3a1ff6d0b6f007f02ecefeeeb6
Ben <thatbenbierens@gmail.com>
2025-10-21 09:12:34 +0200

    Cleanup dht stability test

--- Tests/ArchivistReleaseTests/DHT/DhtStabilityTest.cs
+++ Tests/ArchivistReleaseTests/DHT/DhtStabilityTest.cs
@@ -1,80 +1,107 @@
 ﻿using ArchivistClient;
-using ArchivistPlugin;
 using ArchivistTests;
 using NUnit.Framework;
+using Utils;
 
 namespace ArchivistReleaseTests.DHT
 {
-    [TestFixture(10)]
+    [TestFixture]
     public class DhtStabilityTest : AutoBootstrapDistTest
     {
-        private readonly int numberOfNodes;
-
-        public DhtStabilityTest(int numberOfNodes)
-        {
-            this.numberOfNodes = numberOfNodes;
-        }
+        private int NumberOfNodes => 30;
+        private int MinTotal => NumberOfNodes - 5;
+        private int MinSeen => NumberOfNodes / 2;
+        private int failCount = 0;
+        private DateTime failStartUtc = DateTime.MinValue;
 
         [Test]
         public void RoutingTableStability()
         {
             var duration = TimeSpan.FromHours(24.0);
 
-            var nodes = StartArchivist(numberOfNodes);
+            var nodes = StartArchivist(NumberOfNodes);
+
+            // We take 30 minutes where we don't count problems.
+            // This gives everyone time to discovery everybody.
+            failCount = 0;
+            failStartUtc = DateTime.UtcNow + TimeSpan.FromMinutes(30.0);
+
+            Log($"minTotal: {MinTotal}");
+            Log($"minSeen: {MinSeen}");
+            Log($"failStartUtc: {Time.FormatTimestamp(failStartUtc)}");
 
             WaitAndCheck(nameof(RoutingTableStability),
                 duration,
-                loopTime: TimeSpan.FromMinutes(10),
+                loopTime: TimeSpan.FromSeconds(30),
                 check: () => CheckRoutingTables(nodes));
         }
 
-        private void CheckRoutingTables(IArchivistNodeGroup nodes)
+        private void CheckRoutingTables(IEnumerable<IArchivistNode> nodes)
         {
-            // Each node should have numberOfNodes entries in its routing table.
-            // Each entry should have seen = true, but we will allow 
-            // a low number of seen = false each cycle.
+            // Each node should have numberOfNodes entries in its routing table,
+            // but we allow missing a small number.
 
-            var report = new DhtReport();
+            // Each entry should have seen = true, but we will allow 
+            // a low number of seen = false.
+
+            var report = new DhtReport(
+                minTotal: MinTotal,
+                minSeen: MinSeen
+            );
+
             foreach (var n in nodes)
             {
                 var info = n.GetDebugInfo();
-                CheckRoutingTable(n, info.Table.Nodes, report);
+                report.Add(n.GetName(), info.Table.Nodes);
             }
 
-            report.Publish(Log);
-        }
-
-        private void CheckRoutingTable(IArchivistNode n, DebugInfoTableNode[] nodes, DhtReport report)
-        {
-            var seen = nodes.Count(n => n.Seen);
-            var minSeen = numberOfNodes / 2;
-
-            if (seen >= minSeen) report.AddOK(n.GetName(), seen);
-            else report.AddFail(n.GetName(), seen);
+            report.Publish(Log, onFailed: () =>
+            {
+                if (DateTime.UtcNow < failStartUtc) return;
+                failCount++;
+                Log($"[DHT check failed] ({failCount})");
+                if (failCount > 10) Assert.Fail("One or more node routing tables have degraded.");
+            });
         }
 
         public class DhtReport
         {
-            private readonly List<string> ok = new List<string>();
-            private readonly List<string> failed = new List<string>();
+            private readonly List<string> tags = new List<string>();
+            private readonly int minTotal;
+            private readonly int minSeen;
+            private bool failed = false;
 
-            public void AddOK(string name, int seen)
+            public DhtReport(int minTotal, int minSeen)
             {
-                ok.Add($"{name}={seen}");
+                this.minTotal = minTotal;
+                this.minSeen = minSeen;
             }
 
-            public void AddFail(string name, int seen)
+            public void Add(string name, DebugInfoTableNode[] nodes)
             {
-                failed.Add($"{name}={seen}");
+                var totalOK = nodes.Length >= minTotal;
+                var seenOK = nodes.Count(n => n.Seen) >= minSeen;
+
+                tags.Add($"[{name}=({string.Join(",", 
+                    nodes
+                        .OrderBy(n => n.NodeId)
+                        .Select(n => Tag(n)))})]");
+
+                if (!totalOK || !seenOK) failed = true;
             }
 
-            public void Publish(Action<string> log)
+            private string Tag(DebugInfoTableNode n)
             {
-                log($"[OK] {string.Join(", ", ok)}");
-                if (failed.Any())
+                if (n.Seen) return $"-{n.NodeId}-";
+                return $" {n.NodeId} ";
+            }
+
+            public void Publish(Action<string> log, Action onFailed)
+            {
+                log($"{string.Join(" ", tags)}");
+                if (failed)
                 {
-                    log($"[FAILED] {string.Join(", ", failed)}");
-                    Assert.Fail("One or more node routing tables have degraded.");
+                    onFailed();
                 }
             }
         }
