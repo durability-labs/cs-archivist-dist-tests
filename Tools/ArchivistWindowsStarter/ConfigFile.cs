4e51cf6f9ba9ff281528538e1ce8005ad3cd878d
thatben <thatbenbierens@gmail.com>
2025-11-21 13:24:42 +0100

    Config file and better error handling

---
+++ Tools/ArchivistWindowsStarter/ConfigFile.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace ArchivistWindowsStarter
+{
+    public class ConfigFile
+    {
+        public const string Filename = "config.json";
+
+        public ConfigFile(string version, string network)
+        {
+            Version = version;
+            Network = network;
+        }
+
+        public string Version { get; }
+        public string Network { get; }
+
+        public static ConfigFile? Load()
+        {
+            try
+            {
+                if (!File.Exists(Filename))
+                {
+                    var def = GetDefault();
+                    Save(def);
+                    return def;
+                }
+                var lines = File.ReadAllText(Filename);
+                return JsonConvert.DeserializeObject<ConfigFile>(lines);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static void Save(ConfigFile config)
+        {
+            File.WriteAllText(Filename, JsonConvert.SerializeObject(config));
+        }
+
+        private static ConfigFile GetDefault()
+        {
+            return new ConfigFile(version: "latest", network: "testnet");
+        }
+    }
+}
--- Tools/ArchivistWindowsStarter/Program.cs
+++ Tools/ArchivistWindowsStarter/Program.cs
@@ -14,7 +14,6 @@ namespace ArchivistWindowsStarter
 
         private readonly ILog log;
         private Process? nodeProcess;
-        private const string ArchivistVersion = "v0.0.2";
 
         public Program(string[] args)
         {
@@ -34,17 +33,61 @@ namespace ArchivistWindowsStarter
             catch { }
         }
 
+        private void ShowError(params string[] lines)
+        {
+            Log("Error:");
+            foreach (var l in lines)
+            {
+                Log($"   {l}");
+            }
+            Log("");
+            // Keep the terminal open.
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
         private void Run()
         {
             Log("Initializing...");
 
-            var connector = new ArchivistNetworkConnector(log, version: ArchivistVersion);
-            var config = connector.GetConfig();
+            var config = ConfigFile.Load();
+            if (config == null)
+            {
+                ShowError(
+                    $"   Failed to load configuration from file '{ConfigFile.Filename}'",
+                    "   Make sure the file is correct.",
+                    "   You can delete it to reset to defaults.",
+                    "   If the problem persist, please report it to Durability-labs."
+                );
+                return;
+            }
 
-            Log($"Archivist network: {config.Name}");
-            Log($"Archivist version: {ArchivistVersion}");
+            Log($"Archivist network: {config.Network}");
+            Log($"Archivist version: {config.Version}");
+
+            Log("Fetching network information...");
+            var network = FetchNetworkInfo(config);
+            if (network == null)
+            {
+                ShowError(
+                    $"   Failed to fetch network information.",
+                    "   Make sure the selected network and version values are correct.",
+                    "   You can delete the config file to reset to defaults.",
+                    "   If the problem persist, please report it to Durability-labs."
+                );
+                return;
+            }
 
             var publicIp = GetPublicIP();
+            if (publicIp == null)
+            {
+                ShowError(
+                    $"   Failed determine public IP address.",
+                    "   Please try again later.",
+                    "   If the problem persist, please report it to Durability-labs."
+                );
+                return;
+            }
             Log($"Public IP: {publicIp}");
 
             var datadir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
@@ -64,7 +107,7 @@ namespace ArchivistWindowsStarter
                 "--listen-addrs=/ip4/0.0.0.0/tcp/8070"
             };
 
-            foreach (var spr in config.SPR.Records)
+            foreach (var spr in network.SPR.Records)
             {
                 args.Add($"--bootstrap-node={spr}");
             }
@@ -83,13 +126,17 @@ namespace ArchivistWindowsStarter
             Thread.Sleep(TimeSpan.FromSeconds(3));
             if (nodeProcess != null && nodeProcess.HasExited)
             {
-                Log("Failed to start node...");
-                Log("Error information:");
                 var stdout = nodeProcess.StandardOutput.ReadToEnd();
                 var stderr = nodeProcess.StandardError.ReadToEnd();
-                Log(stdout);
-                Log(stderr);
-                Console.ReadLine();
+
+                ShowError(
+                    "Failed to start node process.",
+                    "Error information:",
+                    stdout,
+                    stderr,
+                    " ",
+                    "Please report this to Durability-labs."
+                );
                 return;
             }
 
@@ -110,14 +157,34 @@ namespace ArchivistWindowsStarter
             }
         }
 
-        private string GetPublicIP()
+        private ArchivistNetwork? FetchNetworkInfo(ConfigFile config)
         {
-            using var httpClient = new HttpClient();
-            var responseTask = httpClient.GetAsync("https://ip.archivist.storage");
-            responseTask.Wait();
-            var stringTask = responseTask.Result.Content.ReadAsStringAsync();
-            stringTask.Wait();
-            return stringTask.Result.Trim();
+            try
+            {
+                var connector = new ArchivistNetworkConnector(log, network: config.Network, version: config.Version);
+                return connector.GetConfig();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private string? GetPublicIP()
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                var responseTask = httpClient.GetAsync("https://ip.archivist.storage");
+                responseTask.Wait();
+                var stringTask = responseTask.Result.Content.ReadAsStringAsync();
+                stringTask.Wait();
+                return stringTask.Result.Trim();
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         private void Log(string msg)
