98549dac43f65a606d2c7a909b3de07192e95926
Ben <thatbenbierens@gmail.com>
2026-03-30 09:57:00 +0200

    Adds test for checking upload fails when reaching quota limit

--- Tests/ArchivistReleaseTests/DataTests/OneClientTest.cs
+++ Tests/ArchivistReleaseTests/DataTests/OneClientTest.cs
@@ -18,6 +18,30 @@ namespace ArchivistReleaseTests.DataTests
             LogNodeStatus(node);
         }
 
+        [Test]
+        public void UploadQuotaLimit()
+        {
+            var node = StartArchivist(n => n.WithStorageQuota(10.MB()));
+
+            for (var i = 0; i < 9; i++)
+            {
+                node.UploadFile(GenerateTestFile(1.MB()));
+                node.Space();
+            }
+
+            try
+            {
+                node.UploadFile(GenerateTestFile(1.MB()));
+                node.Space();
+                Assert.Fail("Successfully uploaded a dataset causing the node to exceed its quota.");
+            }
+            catch (Exception ex)
+            {
+                Assert.That(ex.Message.Contains("Unable to store block"));
+                Log("Upload failed with correct error message.");
+            }
+        }
+
         private void PerformOneClientTest(IArchivistNode primary)
         {
             var testFile = GenerateTestFile(1.MB());
